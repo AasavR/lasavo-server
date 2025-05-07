8781110c0f311689da97f0460c2b5e16e95fa227
AasavR <aasavravi@gmail.com>
2025-05-07 23:45:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+from fastapi import FastAPI, HTTPException, Query
+
+app = FastAPI()
+
+# Example list of authorized user IDs
+authorized_users = {"demo1", "demo2", "demo3"}
+
+@app.get("/api/check_user")
+async def check_user(id: str = Query(..., description="User ID to check")):
+    if id in authorized_users:
+        return "ok"
+    else:
+        raise HTTPException(status_code=403, detail="Unauthorized")
